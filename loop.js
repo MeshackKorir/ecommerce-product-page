2588a5a28117eebfa614170f2720d299565b945e
Meshack Korir <meshackkorir100@gmail.com>
2023-10-12 23:53:45 +0300

    loopng

---
+++ loop.js
@@ -0,0 +1,5 @@
+// Print the first 100 natural numbers Input:No input is provided.Output:First 100 natural numbers are printed. Each integer in a new line.
+
+for(let i = 1; i <= 100; i++) {
+    console.log(i)
+}
---
+++ sum.js
@@ -0,0 +1,12 @@
+// Give me the sum of the prices (try chaining methods) [{price: 10.99}, {price: 5.99}, {price: 29.99}]
+
+const nums = [{price: 10.99}, {price: 5.99}, {price: 29.99}]
+
+const totalSum = nums
+    .map(num => num.price)
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    
+    console.log(totalSum)
+
+
+    
